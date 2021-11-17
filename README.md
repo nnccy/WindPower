821b25757bc0fe6b6c8aaac29da07d8c05cab290
nnccy <1092881861@qq.com>
2021-11-17 22:50:14 +0800

    加注释

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # WindPower
 
+
+- --sava 可视化结果、日志、保存的模型文件目录
+-  --mymodel 你选择用的模型，在程序中用if区分
 ```bash
 CUDA_VISIBLE_DEVICES=6 python main.py --save  dir  --mymodel lstm
 ```
--- main.py
+++ main.py
@@ -75,7 +75,7 @@ logging('Random seed', paras.seed)
 dm = Data_Manager()
 dm.load_train_data() #加载训练集
 dm.load_test_data() #加载测试集
-#将不存在的数据转成num
+#将不存在的数据转成0，现在训练集的数据有些地方不存在，强制转成0了都
 np.nan_to_num(dm.X_, copy=False)
 np.nan_to_num(dm.X0, copy=False)
 np.nan_to_num(dm.Y0, copy=False)
@@ -86,35 +86,48 @@ np.nan_to_num(dm.test_X0, copy=False)
 np.nan_to_num(dm.test_W, copy=False)
 times = 1
 dm.generate_indexes(times) #验证集索引
-dm.generate_dev() #生成验证集和验证集结果
+dm.generate_dev() #生成验证集和验证集结果以及验证集索引，写入csv文件。
 
 "++++++++++++++++++++++++++++++++++围起来的地儿是为了说明数据的访问方法，以及可视化的使用，可以注释掉+++++++++++++++++++++++++++++++++++++++++++"
 
 print("-----------------------根据索引遍历训练集----------------------------")
 # 训练集遍历功率温度，风向风速
+#用法,
 for f in range(field_len):
-    indexes = dm.get_indexes(f)  #取训练集索引，并随机化
-    print(field[f], dm.X_[f, indexes].shape) #功率温度
-    print(field[f], dm.X0[f, indexes].shape) #风向风速
-    print(field[f], dm.W[f, indexes].shape)  # 对应的气象数据
-    print(field[f], dm.Y0[f, indexes].shape) #输出的风向风速
+    # 可以看到，这就是无放回的抽样，每个风场训练集共有17507个时段，每次取出春夏秋冬各20段，也就是共80段，这80段是25个风场的数据，每次索引共能取出80*25共2000个时段，用以训练。。。一共能取17507/80次的索引,看来数据也不少
+    #我们要做的就是综合输入 dm.X_[f, indexes]，dm.X0[f, indexes]，dm.W[f, indexes].选择合适的模型，来预测y（未来10分钟的风速，风向）, 并与dm.Y0[f, indexes]求loss,优化
+    indexes = dm.get_indexes(f)  #取训练集索引，并随机化,
+    indexes1 = dm.get_indexes(f)
+    indexes2 = dm.get_indexes(f)
+    print(indexes)
+    print(field[f], dm.X_[f, indexes].shape) #功率 温度  风场f (80, 25, 120, 2)
+    print(field[f], dm.X0[f, indexes].shape) #风速 风向 风场f (80, 25, 120, 2)
+    print(field[f], dm.W[f, indexes].shape)  # 对应的气象数据（风速风向，未归一化）  风场f (80, 14, 2)，共13个小时的数据，也就是14个值，前12个值是过去的，后两个是未来的 ,所有风机都是用的这个
+
+    print(field[f], dm.Y0[f, indexes].shape) #输出的风向风速   风场1 (80, 25, 20, 2)
 print("----------------------遍历测试集-----------------------------")
 
-#测试集遍历
-for f in range(field_len):
-    for p in range(period_len):
-        for m in range(machine_len):
-            print(field[f], period[p], machine[f][m], dm.test_X0[f, p, m].shape)
+#测试集按照这种方式遍历，最后将测试集中的数据，输入到训练好的模型中，得出结果，写入到文件中，就是我们要提交的答案，直接看print输出应该好理解
+for f in range(field_len):   #风场
+    for p in range(period_len):  #时段
+        for m in range(machine_len):  #机器
+            print(field[f], period[p], machine[f][m], dm.test_X_[f, p, m].shape) #（功率 温度）  print 风场f 时段？  风机号  (120, 2)
+            print(field[f], period[p], machine[f][m], dm.test_X0[f, p, m].shape)#（风速 风向）   print 风场f 时段？  风机号  (120, 2)
+            print(field[f], period[p], dm.test_W[f, p].shape)  # 对应的气象数据（风速风向，未归一化）  print 风场f 时段？   (14, 2),一个风场所有风机都是用的这个
+
 print("-----------------------遍历验证集----------------------------")
-#验证集遍历
+#验证集遍历，只有这80个时段,用于我们评估
 val_indexes_df = read_csv('./data/val_indexes.csv')
 val_indexes = {field[f]: {0: val_indexes_df[field[f]].values} for f in range(field_len)}
 for f in range(field_len):
     indexes = dm.get_indexes(f)
-    print(field[f], dm.X0[f, indexes].shape)
+    print(field[f], dm.X_[f, indexes].shape)  # 功率温度  风场1 (80, 25, 120, 2)
+    print(field[f], dm.X0[f, indexes].shape)  # 风速 风向 风场1 (80, 25, 120, 2)
+    print(field[f], dm.W[f, indexes].shape)  # 对应的气象数据（风速风向，未归一化）  风场1 (80, 14, 2)，共13个小时的数据，也就是14个值，前12个值是过去的，后两个是未来的
+    print(field[f], dm.Y0[f, indexes].shape)  # 输出的风向风速   风场1 (80, 25, 20, 2)
 
 print("-----------------------根据验证集，用随机方式生成预测结果，并可视化----------------------------")
-
+#生成的预测结果与验证集的真实数据，做可视化对比（数据来源与  预测值dev_pred.csv和真实值dev_true.csv)，并计算分数，都在这一段
 #生成验证集结果并保存，用mean+std的方式
 t = 0    # 第 t 个验证集，不要超过你设定的最大值
 df = pd.DataFrame(
@@ -146,9 +159,9 @@ for f in range(field_len):
 val_pred.loc[:, ['风速', '风向']] = all
 val_pred["风速"] = val_pred["风速"].astype('float64')
 val_pred["风向"] = val_pred["风向"].astype('float64')
-val_pred.to_csv('./data/dev_pred.csv', float_format='%.4f', index=False, encoding='utf-8')
-logging('Score', cal_R())#计算得分
-Compare_dev_result(dm,paras.save)
+val_pred.to_csv('./data/dev_pred.csv', float_format='%.4f', index=False, encoding='utf-8')  #生成随机结果，写入验证集预测文件
+logging('Score', cal_R())        # 计算得分
+Compare_dev_result(dm,paras.save) # 可视化比较
 
 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 if paras.mymodel == 'lstm':
